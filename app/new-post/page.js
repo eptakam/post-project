eabb267015113584944e2eb92c6006b5e0bdb97b
eptakam <emmataks@gmail.com>
2024-07-04 10:56:24 -0300

    Providing user feedback with the useFormStatus hook.

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,18 +1,33 @@
+import FormSubmit from '@/components/form-submit';
 import { storePost } from '@/lib/posts';
+import { redirect } from 'next/navigation';
+
+/*
+    Note:
+      en React, nous avons deux options pour gérer les formulaires: 'form action' et 'Server actions'
+
+      - 'form action': une fonction est creee soit dans un fichier separe ou dans le meme fichier pour traiter les donnees du formulaire. et cette fonction est appelee dans l'attribut 'action' du formulaire. cette fonction recoit automatiquement un objet 'formData' qui contient les donnees du formulaire.
+
+      - 'Server actions': une fonction est creee dans un fichier separe pour traiter les donnees du formulaire. cette fonction recoit les donnees du formulaire en parametre. cette fonction est appelee dans le composant pour traiter les donnees du formulaire. il est important d'ajouter 'use server' au debut de la fonction pour creer un server action. et lors la fontion doit devenir une fonction asynchrone en ajoutant 'async' avant le mot cle 'function'.
+*/
 
 export default function NewPostPage() {
   async function createPost(formData) {
-    "use server";
+    "use server"; // creer un server action
     const title = formData.get('title');
     const image = formData.get('image');
     const content = formData.get('content');
 
-    storePost({
+    // appeler la fonction 'storePost' qui est dans le fichier 'lib/posts.js' pour enregistrer le post
+    await storePost({
+      // nous avons ajoute le await pour attendre que la fonction 'storePost' soit terminee avant de rediriger l'utilisateur et surtout porce que nous voulons utiliser un loading fallback pendant le traitement du formulaire
       imageUrl: '',
-      title,
-      content,
+      title,  // ou title: title. title, est possible car le nom de la variable est le meme que le nom de la propriete (key)
+      content,  // ou content: content
       userId: 1
-    })
+    });
+
+    redirect('/feed'); // rediriger l'utilisateur vers la page feed
   }
 
   return (
@@ -37,8 +52,7 @@ export default function NewPostPage() {
           <textarea id="content" name="content" rows="5" />
         </p>
         <p className="form-actions">
-          <button type="reset">Reset</button>
-          <button>Create Post</button>
+          <FormSubmit />
         </p>
       </form>
     </>
---
+++ components/form-submit.js
@@ -0,0 +1,22 @@
+/*
+    ceci nous permettra de remplacer le bouton de soumission du formulaire pour avoir un loading fallback pendant le traitement du formulaire
+*/
+'use client';
+
+import { useFormStatus } from "react-dom";
+
+export default function FormSubmit({ children, onSubmit }) {
+  const status = useFormStatus(); // ne peut etre utilise que dans un composant client
+
+  if (status.pending) {
+    return <p>Creating post...</p>;
+  }
+  
+  return (
+    <>
+    {/* mettre le/les boutons a remplacer ici... */}
+      <button type="reset">Reset</button>
+      <button>Create Post</button>
+    </>
+  );
+}
