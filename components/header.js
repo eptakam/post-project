0ba59689538853df8288996eddd585578cc711a2
eptakam <emmataks@gmail.com>
2024-07-10 14:38:51 -0300

    Controlling the image size>

--- components/header.js
+++ components/header.js
@@ -1,11 +1,21 @@
-import logo from '@/assets/logo.png';
-import Link from 'next/link';
+import logo from "@/assets/logo.png";
+import Image from "next/image";
+
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header id="main-header">
       <Link href="/">
-        <img src={logo.src} alt="Mobile phone with posts feed on it" />
+        {/* <img src={logo.src} alt="Mobile phone with posts feed on it" /> */}
+        <Image
+          src={logo}  
+          // definir le width et height lorsqu'on aura une taille d'image fixe peu importe la taille de l'ecran
+          width={100}
+          height={100}
+          // sizes="10vw"  // 10% of the viewport width. pratique recommandee pour l'optimisation de l'image
+          alt="Mobile phone with posts feed on it"
+        />
       </Link>
       <nav>
         <ul>
@@ -13,7 +23,9 @@ export default function Header() {
             <Link href="/feed">Feed</Link>
           </li>
           <li>
-            <Link className='cta-link' href="/new-post">New Post</Link>
+            <Link className="cta-link" href="/new-post">
+              New Post
+            </Link>
           </li>
         </ul>
       </nav>
